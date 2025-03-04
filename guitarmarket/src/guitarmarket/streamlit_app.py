d4f3b1f25c672ae6964a727f4c5c7cfcc1916fcf
JamesJansma <jjansma@zagmail.gonzaga.edu>
2025-03-04 10:09:27 -0800

    Adding streamlit app

---
+++ guitarmarket/src/guitarmarket/streamlit_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from guitarmarket.crew import Guitarmarket
+import os
+
+st.title('Your Guitar Price Comparison')
+
+
+with st.sidebar:
+    st.header('Enter FB Login')
+    topic = st.text_input("Username")
+    detailed_questions = st.text_area("Password")
+
+if st.button('Run Research'):
+    if not topic or not detailed_questions:
+        st.error("Please fill all the fields.")
+    else:
+        inputs = {
+            'topic' : 'Guitar'
+        }
+        try:
+            result = Guitarmarket().crew().kickoff(inputs=inputs)
+        except Exception as e:
+            raise Exception(f"An error occurred while running the crew: {e}")
+        st.subheader("Results of your research project:")
+        st.write(result.raw)
